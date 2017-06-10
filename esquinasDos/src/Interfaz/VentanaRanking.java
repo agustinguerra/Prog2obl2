6b5f840a1d50d2ac5a3a642591fc81eb63df953f
agustinguerra <agustin.guerraguerra8@gmail.com>
2017-06-10 16:48:30 -0300

    Leer descripcion
    Se arreglo el merge.
    
    Se arreglo el ranking, completamente funcional.

--- esquinasDos/src/Interfaz/VentanaRanking.java
+++ esquinasDos/src/Interfaz/VentanaRanking.java
@@ -1,9 +1,12 @@
 package Interfaz;
 
 import Dominio.Sistema;
+import Persistencia.ArchivoExcel;
 import java.util.Collections;
 import java.util.Observable;
 import java.util.Observer;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 public class VentanaRanking extends javax.swing.JFrame implements Observer {
 
@@ -14,7 +17,8 @@ public class VentanaRanking extends javax.swing.JFrame implements Observer {
         objSistema = modelo;
         initComponents();
         objSistema.addObserver(this);
-        actualizar();
+        this.nombreExcel.setText("");
+        actualizar();   
     }   
         
     @Override
@@ -26,14 +30,28 @@ public class VentanaRanking extends javax.swing.JFrame implements Observer {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        noHayNombre = new javax.swing.JDialog();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         btnGenerar = new javax.swing.JButton();
         btnVolver = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
         jScrollPane2 = new javax.swing.JScrollPane();
         listaPartidas = new javax.swing.JList();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        nombreExcel = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+
+        javax.swing.GroupLayout noHayNombreLayout = new javax.swing.GroupLayout(noHayNombre.getContentPane());
+        noHayNombre.getContentPane().setLayout(noHayNombreLayout);
+        noHayNombreLayout.setHorizontalGroup(
+            noHayNombreLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        noHayNombreLayout.setVerticalGroup(
+            noHayNombreLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -57,7 +75,7 @@ public class VentanaRanking extends javax.swing.JFrame implements Observer {
             }
         });
         jPanel1.add(btnGenerar);
-        btnGenerar.setBounds(380, 200, 140, 40);
+        btnGenerar.setBounds(240, 330, 130, 40);
 
         btnVolver.setBackground(new java.awt.Color(0, 0, 0));
         btnVolver.setFont(new java.awt.Font("Segoe Print", 1, 18)); // NOI18N
@@ -70,32 +88,30 @@ public class VentanaRanking extends javax.swing.JFrame implements Observer {
             }
         });
         jPanel1.add(btnVolver);
-        btnVolver.setBounds(400, 290, 100, 40);
-
-        jTextField1.setText("Para Generar un archivo Excel con el ranking de los jugadores presione \"Generar\". ");
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jTextField1);
-        jTextField1.setBounds(10, 70, 480, 40);
-
-        jTextField2.setText("Para volver al menu principal, presione \"Volver\".");
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jTextField2);
-        jTextField2.setBounds(10, 110, 480, 40);
+        btnVolver.setBounds(380, 330, 90, 40);
 
         listaPartidas.setBackground(new java.awt.Color(3, 5, 27));
         listaPartidas.setForeground(new java.awt.Color(255, 255, 255));
         jScrollPane2.setViewportView(listaPartidas);
 
         jPanel1.add(jScrollPane2);
-        jScrollPane2.setBounds(20, 180, 380, 190);
+        jScrollPane2.setBounds(20, 150, 450, 160);
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jLabel2.setText("Para generar un archivo Excel con el ranking de los jugadores, presione \"Generar\".");
+        jPanel1.add(jLabel2);
+        jLabel2.setBounds(20, 70, 460, 30);
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jLabel3.setText(" Para volver al menu principal, presione \"Volver\".");
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(20, 110, 300, 30);
+        jPanel1.add(nombreExcel);
+        nombreExcel.setBounds(120, 330, 130, 30);
+
+        jLabel4.setText("Nombre del archivo");
+        jPanel1.add(jLabel4);
+        jLabel4.setBounds(20, 340, 100, 14);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -113,32 +129,47 @@ public class VentanaRanking extends javax.swing.JFrame implements Observer {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnGenerarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGenerarActionPerformed
-        //ACA CUANDO SE APRETA EL BOTON GENERAR, GENERAR EXCEL
-        dispose();
+        if (this.nombreExcel.getText().equals("") == false) {
+            ArchivoExcel archExcel = new ArchivoExcel();
+            JFileChooser chooser = new JFileChooser();
+            chooser.setCurrentDirectory(new java.io.File("."));
+            chooser.setDialogTitle("Selecciona la carpeta destino");
+            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            //
+            // disable the "All files" option.
+            //
+            chooser.setAcceptAllFileFilterUsed(false);
+            //    
+            if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+                String pathElegido = chooser.getCurrentDirectory().toString();
+                String fileElegido = chooser.getSelectedFile().toString();             
+                archExcel.crearExcel(objSistema, nombreExcel.getText(), fileElegido);
+            } else {
+                JOptionPane.showMessageDialog(null, "No se selecciono un destino correcto.");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "No se selecciono el nombre del archivo a guardar.");
+        }
+        //dispose(); NO ES NECESARIO HACER ESTE DISPOSE PORQUE SE TE CIERRA LA VENTANA ENTERA Y ESTO ES SOLO PARA CREAR EL EXCEL
     }//GEN-LAST:event_btnGenerarActionPerformed
 
     private void btnVolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVolverActionPerformed
         dispose();
     }//GEN-LAST:event_btnVolverActionPerformed
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
-
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField2ActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnGenerar;
     private javax.swing.JButton btnVolver;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
     private javax.swing.JList listaPartidas;
+    private javax.swing.JDialog noHayNombre;
+    private javax.swing.JTextField nombreExcel;
     // End of variables declaration//GEN-END:variables
 
     public void actualizar() {
--- esquinasDos/src/Persistencia/ArchivoExcel.java
+++ esquinasDos/src/Persistencia/ArchivoExcel.java
@@ -1,5 +1,46 @@
 package Persistencia;
 
+import Dominio.Sistema;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
 public class ArchivoExcel {
-    
+
+    public void crearExcel(Sistema sis, String nombreArchivo,String path) {
+        try {
+            //PARA HACER LA PARTE DEL NOMBRE Y LA UBICACION, TENGO QUE HACERLO CUANDO ESTEN LAS VENTANAS HECHAS, SI LO HAGO ACA ES CODIGO QUE DESPUES NO SIRVE
+            String filename = nombreArchivo + ".xls";
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet sheet = workbook.createSheet("Ranking");
+            int cantidad; //ACA ME FIJO CUANTAS PERSONAS HAY PARA VER CUANTAS COLUMNAS TENGO QUE CREAR EN EL EXCEL.
+            cantidad = sis.cantidadJugadores();
+            HSSFRow rowhead = sheet.createRow((short) 0);
+            rowhead.createCell(0).setCellValue("Nombre");
+            rowhead.createCell(1).setCellValue("Edad");
+            rowhead.createCell(2).setCellValue("Cantidad de partidas ganadas");
+
+            for (int i = 0; i < cantidad; i++) {
+                HSSFRow row = sheet.createRow((short) i + 1);
+                row.createCell(0).setCellValue((sis.getListaJugadores().get(i)).getNombre());
+                row.createCell(1).setCellValue((sis.getListaJugadores().get(i)).getEdad());
+                row.createCell(2).setCellValue((sis.getListaJugadores().get(i)).getJuegosGanados());
+            }
+
+            FileOutputStream fileOut = new FileOutputStream(path+"\\"+filename);
+            System.out.println(path);
+            System.out.println(filename);
+            workbook.write(fileOut);
+            fileOut.close();
+            System.out.println("El archivo de excel fue correctamente creado.");
+
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public ArchivoExcel() {
+    }
 }
